3c4af892291280beeefea60479d7973335a54fce
Jason Deng <116106794+demardefrozen10@users.noreply.github.com>
2023-09-20 17:56:02 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+def coloured_circle(color_choice):
+    return color_choice
+
+
+myColors = ['brown', 'yellow', 'pink', 'blue']
+for colors in myColors:
+    coloured_circle(colors)
+
+coloured_circle()
