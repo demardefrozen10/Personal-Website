4a61f9db2f21c8bc9e3da4b8b9398b027200af42
Jason Deng <116106794+demardefrozen10@users.noreply.github.com>
2023-10-18 01:48:29 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-def coloured_circle(color_choice):
-    return color_choice
-
-
-myColors = ['brown', 'yellow', 'pink', 'blue']
-for colors in myColors:
-    coloured_circle(colors)
-
-coloured_circle()
